b196be6841e323bfc9f38f5c29d4747081d033d9
Xelera Communication <info@xelera.se>
2012-04-11 12:13:55 +0200

    Port to PHP

---
+++ src/coordinatetransformation/Position.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *  CoordinateTransformationLibrary - Mathias Åhsberg 2009
+ *
+ *  RT90, SWEREF99 and WGS84 coordinate transformation library
+ *
+ * Visit my repository at http://github.com/goober
+ *
+ * This library is a java port of the .NET library by Björn Sållarp.
+ *  calculations are based entirely on the excellent
+ *  javscript library by Arnold Andreassons.
+ *
+ * Source: http://www.lantmateriet.se/geodesi/
+ * Source: Arnold Andreasson, 2007. http://mellifica.se/konsult
+ * Source: Björn Sållarp. 2009. http://blog.sallarp.com
+ * Author: Mathias Åhsberg, 2009. http://github.com/goober/
+ *
+ * License: http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
+abstract class Grid {
+  const RT90 = 0;
+  const WGS84 = 1;
+  const SWEREF99 = 3;
+}
+
+abstract class Position {
+
+    //public enum Grid {RT90,WGS84,SWEREF99}
+
+    protected $latitude;
+    protected $longitude;
+    protected $gridFormat;
+
+    public function __construct() {
+      $args = func_get_args();
+      if(count($args) == 3) {
+        call_user_func_array(array($this, 'setPositionLatLonFormat'), $args);
+      }
+      else if(count($args) == 1) {
+        call_user_func_array(array($this, 'setPositionFormat'), $args);
+      }
+    }
+
+    private function setPositionLatLonFormat($lat, $lon, $format) {
+      $this->latitude = $lat;
+      $this->longitude = $lon;
+      //@todo: validation?
+      $this->gridFormat = $format;
+    }
+
+    private function setPositionFormat($format) {
+      //@todo: validation?
+      $this->gridFormat = $format;
+    }
+
+    public function getLatitude() {
+        return $this->latitude;
+    }
+    public function getLongitude() {
+        return $this->longitude;
+    }
+}
--- src/coordinatetransformation/positions/ParseException.php
+++ src/coordinatetransformation/positions/ParseException.php
@@ -1,3 +1,4 @@
+<?php
 /**
  *  CoordinateTransformationLibrary - Mathias Åhsberg 2009
  *
@@ -17,33 +18,11 @@
  * License: http://creativecommons.org/licenses/by-nc-sa/3.0/
  */
 
-package coordinatetransformation;
+//package coordinatetransformation.positions;
 
-
-public abstract class Position {
-
-    public enum Grid {RT90,WGS84,SWEREF99}
-
-    protected double latitude;
-    protected double longitude;
-    protected Grid gridFormat;
-
-    public Position(double lat, double lon,Grid format) {
-        latitude = lat;
-        longitude = lon;
-        gridFormat = format;
-    }
-    public Position(Grid format) {
-        gridFormat = format;
-    }
-    public Position() {
-        
-    }
-
-    public double getLatitude() {
-        return this.latitude;
-    }
-    public double getLongitude() {
-        return this.longitude;
-    }
+/**
+ * Signals that an error has been reached unexpectedly while parsing. 
+ * 
+ */
+class ParseException extends Exception {
 }
